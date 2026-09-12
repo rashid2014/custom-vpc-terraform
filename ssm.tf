00e6396453d347683b0f9bc8de0568d581a1a84a
Harun Ur Rashid <harunnec@gmail.com>
2026-09-11 21:19:40 -0400

    Deploy Custom VPC

---
+++ ssm.tf
@@ -0,0 +1,36 @@
+locals {
+    ssm_parameters = {
+        vpc_id = {
+            name = /pragra/project-a/vpc/id
+            value = aws_vpc.custom_vpc.id
+        }
+        publicRT_id = {
+            name = /pragra/project-a/publicrt/id
+            value = aws_route_table.public_rt.id
+        }
+        subnet1_id = {
+            name = /pragra/project-a/subnet1/id
+            value = aws_subnet.subnet1.id
+        }
+        subnet2_id = {
+            name = /pragra/project-a/subnet2/id
+            value = aws_subnet.subnet2.id
+        }
+        subnet3_id = {
+            name = /pragra/project-a/subnet3/id
+            value = aws_subnet.subnet3.id
+        }
+        subnet4_id = {
+            name = /pragra/project-a/subnet4/id
+            value = aws_subnet.subnet4.id
+        } 
+    }
+}
+
+resource "aws_ssm_parameter" "resources" {
+  for_each = local.ssm_parameters
+
+  name  = each.value.name
+  type  = "String"
+  value = each.value.value
+}
--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,14 @@
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.2"
+  backend "s3" {
+    bucket = "pragra-tfstate-2026"
+    key    = "env/dev/terraform.tfstate"
+    region = "ca-central-1"
+  }
 }
