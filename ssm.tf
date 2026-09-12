09d2bc4463e744faec9687c1fea325bebf78365d
Harun Ur Rashid <harunnec@gmail.com>
2026-09-11 21:24:46 -0400

    Deploy Custom VPC

--- ssm.tf
+++ ssm.tf
@@ -1,27 +1,27 @@
 locals {
     ssm_parameters = {
         vpc_id = {
-            name = /pragra/project-a/vpc/id
+            name = "/pragra/project-a/vpc/id"
             value = aws_vpc.custom_vpc.id
         }
         publicRT_id = {
-            name = /pragra/project-a/publicrt/id
+            name = "/pragra/project-a/publicrt/id"
             value = aws_route_table.public_rt.id
         }
         subnet1_id = {
-            name = /pragra/project-a/subnet1/id
+            name = "/pragra/project-a/subnet1/id"
             value = aws_subnet.subnet1.id
         }
         subnet2_id = {
-            name = /pragra/project-a/subnet2/id
+            name = "/pragra/project-a/subnet2/id"
             value = aws_subnet.subnet2.id
         }
         subnet3_id = {
-            name = /pragra/project-a/subnet3/id
+            name = "/pragra/project-a/subnet3/id"
             value = aws_subnet.subnet3.id
         }
         subnet4_id = {
-            name = /pragra/project-a/subnet4/id
+            name = "/pragra/project-a/subnet4/id"
             value = aws_subnet.subnet4.id
         } 
     }
